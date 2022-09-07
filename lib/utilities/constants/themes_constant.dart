9ba43e535f17e0384c102f00ef0ae01429dd85f0
Chiekko Red <chiekkored@gmail.com>
2022-09-07 17:50:01 +0800

    Public theme variables

--- lib/utilities/constants/themes_constant.dart
+++ lib/utilities/constants/themes_constant.dart
@@ -4,44 +4,44 @@ import 'package:flutter/material.dart';
 class CColors {
   /// SECTION lightTheme
   static ThemeData lightTheme = ThemeData(
-      scaffoldBackgroundColor: _scaffoldLightBackgroundColor,
-      backgroundColor: _containerLightBackgroundColor,
+      scaffoldBackgroundColor: scaffoldLightBackgroundColor,
+      backgroundColor: containerLightBackgroundColor,
 
       /// SECTION outlinedButtonTheme
       outlinedButtonTheme: OutlinedButtonThemeData(
           style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(_buttonLightColor))),
+              backgroundColor: MaterialStateProperty.all(buttonLightColor))),
 
       /// !SECTION
       /// SECTION textTheme
       textTheme: const TextTheme(
         bodyText1: TextStyle(
-            color: _primaryTextLightColor,
+            color: primaryTextLightColor,
             fontSize: 17.0,
             fontWeight: FontWeight.w500,
             fontFamily: "Inter"),
         bodyText2: TextStyle(
-            color: _primaryTextLightColor,
+            color: primaryTextLightColor,
             fontSize: 15.0,
             fontWeight: FontWeight.w500,
             fontFamily: "Inter"),
         subtitle1: TextStyle(
-            color: _primaryTextLightColor,
+            color: primaryTextLightColor,
             fontSize: 12.0,
             fontWeight: FontWeight.w500,
             fontFamily: "Inter"),
         headline1: TextStyle(
-            color: _primaryTextLightColor,
+            color: primaryTextLightColor,
             fontSize: 22.0,
             fontWeight: FontWeight.w700,
             fontFamily: "Inter"),
         headline2: TextStyle(
-            color: _primaryTextLightColor,
+            color: primaryTextLightColor,
             fontSize: 17.0,
             fontWeight: FontWeight.w700,
             fontFamily: "Inter"),
         headline3: TextStyle(
-            color: _primaryTextLightColor,
+            color: primaryTextLightColor,
             fontSize: 15.0,
             fontWeight: FontWeight.w700,
             fontFamily: "Inter"),
@@ -51,17 +51,17 @@ class CColors {
       /// SECTION colorScheme
       colorScheme: ColorScheme.fromSwatch().copyWith(
         // Main Colors
-        primary: _buttonLightColor,
-        secondary: _secondaryColor,
-        outline: _strokeColor,
-        error: _dangerColor,
-        surface: _onlineColor,
+        primary: buttonLightColor,
+        secondary: secondaryColor,
+        outline: strokeColor,
+        error: dangerColor,
+        surface: onlineColor,
         // Container Colors
-        primaryContainer: _buttonLightColor,
-        secondaryContainer: _secondaryButtonLightColor,
-        errorContainer: _dangerColor,
+        primaryContainer: buttonLightColor,
+        secondaryContainer: secondaryButtonLightColor,
+        errorContainer: dangerColor,
         // Text Colors
-        tertiary: _secondaryTextLightColor,
+        tertiary: secondaryTextLightColor,
       )
 
       /// !SECTION
@@ -71,13 +71,13 @@ class CColors {
 
   /// SECTION darkTheme
   static ThemeData darkTheme = ThemeData(
-    scaffoldBackgroundColor: _scaffoldDarkBackgroundColor,
-    backgroundColor: _containerDarkBackgroundColor,
+    scaffoldBackgroundColor: scaffoldDarkBackgroundColor,
+    backgroundColor: containerDarkBackgroundColor,
 
     /// SECTION outlinedButtonTheme
     outlinedButtonTheme: OutlinedButtonThemeData(
         style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(_buttonDarkColor))),
+            backgroundColor: MaterialStateProperty.all(buttonDarkColor))),
 
     /// !SECTION
     /// SECTION textTheme
@@ -93,23 +93,23 @@ class CColors {
 
   /// ANCHOR Common colors constants
   static const white = Color.fromRGBO(223, 223, 223, 1);
-  static const _black = Color.fromRGBO(19, 19, 19, 1);
+  static const black = Color.fromRGBO(19, 19, 19, 1);
 
   /// ANCHOR Light Theme constants
-  static const _scaffoldLightBackgroundColor = Color.fromRGBO(169, 169, 169, 1);
-  static const _containerLightBackgroundColor =
+  static const scaffoldLightBackgroundColor = Color.fromRGBO(169, 169, 169, 1);
+  static const containerLightBackgroundColor =
       Color.fromARGB(255, 255, 255, 255);
-  static const _buttonLightColor = Color(0xFF5A189A);
-  static const _secondaryButtonLightColor = Color(0xFFF4F5F7);
-  static const _primaryTextLightColor = Color(0xFF2E3E5C);
-  static const _secondaryTextLightColor = Color(0xFF9FA5C0);
-  static const _secondaryColor = Color(0xFFFF8500);
-  static const _dangerColor = Color(0xFFE53F71);
-  static const _strokeColor = Color(0xFFD0DBEA);
-  static const _onlineColor = Color(0xFF34C300);
+  static const buttonLightColor = Color(0xFF5A189A);
+  static const secondaryButtonLightColor = Color(0xFFF4F5F7);
+  static const primaryTextLightColor = Color(0xFF2E3E5C);
+  static const secondaryTextLightColor = Color(0xFF9FA5C0);
+  static const secondaryColor = Color(0xFFFF8500);
+  static const dangerColor = Color(0xFFE53F71);
+  static const strokeColor = Color(0xFFD0DBEA);
+  static const onlineColor = Color(0xFF34C300);
 
   /// ANCHOR Dark Theme constants
-  static const _scaffoldDarkBackgroundColor = Color.fromARGB(255, 29, 29, 29);
-  static const _containerDarkBackgroundColor = Color.fromARGB(255, 45, 45, 45);
-  static const _buttonDarkColor = Color.fromRGBO(25, 124, 40, 1);
+  static const scaffoldDarkBackgroundColor = Color.fromARGB(255, 29, 29, 29);
+  static const containerDarkBackgroundColor = Color.fromARGB(255, 45, 45, 45);
+  static const buttonDarkColor = Color.fromRGBO(25, 124, 40, 1);
 }
